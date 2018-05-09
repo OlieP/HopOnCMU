fe54f48e76de32b03a53272898861ef1526f764e
peolie <pe.m.oliveira@gmail.com>
2018-05-09 17:30:35 +0100

    fist

---
+++ src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/server/MainServer.java
@@ -0,0 +1,8 @@
+package pt.ulisboa.tecnico.cmov.hoponcmu.server;
+
+public class MainServer {
+	public static void main(String[] args)
+			throws Exception {
+		new Server().start();
+}
+}
---
+++ src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/server/ServerReply.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.cmov.hoponcmu.server;
+
+public enum ServerReply {
+	SUCESS,
+	
+	//login
+	WRONG_USER,
+	WRONG_PASS,
+	
+	//sign up
+	INVALID_USER,
+	INVALID_PASS,
+	
+}
